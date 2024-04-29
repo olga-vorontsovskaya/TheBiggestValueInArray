ff1e634ec71ed5dad8ebcdd2c7adce58a6383880
olga-vorontsovskaya <o.vorontsovskaya@gmail.com>
2024-04-29 16:38:31 +0300

    Add project files.

---
+++ TheBiggestNumberInArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBiggestNumberInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 54, 9, 7843, 234, 890, 65192, 31, 112847 };
+
+            int maxValue = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > maxValue)
+                    maxValue = array[i];
+            }
+
+            Console.WriteLine(maxValue);
+
+        }
+    }
+}
